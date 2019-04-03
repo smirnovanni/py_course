3c6684b95086235f36550323097289f191c3782a
Антон Смирнов <a.smirnov@modulbank.ru>
2019-04-03 17:53:53 +0300

    fssp_api

--- fssprusbot/fssp_api.py
+++ fssprusbot/fssp_api.py
@@ -3,7 +3,6 @@ import requests
 API_KEY = 'cHGk0u8XIFM1'
 API_URL = 'https://api-ip.fssprus.ru/api/v1.0'
 
-
 def send_req(**kwargs):
     region = kwargs['region']
     firstname = kwargs['firstname']
@@ -30,15 +29,8 @@ def get_status(task):
 
 def get_result(task):
     req = {'token': API_KEY, 'task': task}
-    answer = requests.get(API_URL + '/status', req)
+    answer = requests.get(API_URL + '/result', req)
     if answer.json()['status'] == 'success':
         return answer.json()['response']
     else:
         return answer.json()['exception']
-
-#r = send_req(region=None, firstname='Сергей', lastname='Соколов', secondname=None, birthdate=None)
-#print(r)
-#d = get_status(r)
-#print(d)
-f = get_result('e08a075b-6f5e-4386-b3ba-2948ad6657bd')
-print(f)
--- fssprusbot/fssprusbot.py
+++ fssprusbot/fssprusbot.py
@@ -1,5 +1,10 @@
+import sqlite3
+from datetime import datetime
+
 from telebot import TeleBot
-import fssp_api
+
+from fssp_api import *
+
 
 def run_bot():
     bot = TeleBot('719958666:AAEmt0CT6CJS_xZvChdSXlb3RnipzzYK214')
@@ -10,9 +15,41 @@ def run_bot():
 
     @bot.message_handler(content_types=['text'])
     def function_name(message):
-        bot.send_message(message.chat.id, message.text)
+        db = sqlite3.connect("fssp_api_bot.db")
+        region, firstname, lastname, secondname, birthdate = str(message.text).split(' ')
+        task_id = send_req(region=region, firstname=firstname, lastname=lastname, secondname=secondname,
+                           birthdate=birthdate)
+        try:
+            cursor = db.cursor()
+            cursor.execute("""INSERT INTO queue({columns}) VALUES (?, ?, ?, ?)
+            """.format(columns='task_id, date, state, chat_id'),
+                           (task_id, datetime.now().strftime('%d.%m.%Y'), 0, message.chat.id))
+            db.commit()
+            db.close()
+        except:
+            bot.send_message(message.chat.id, "Попробуйте позже")
+        finally:
+            bot.register_next_step_handler(message, get_results)
 
-    bot.polling(none_stop=True)
+    def get_results(message):
+        while True:
+            db = sqlite3.connect("fssp_api_bot.db")
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM queue WHERE state=0 and chat_id=?", (message.chat.id,))
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+                state = get_status(row[1])
+                if state == 0:
+                    msg = get_result(row[1])
+                    print(msg)
+                    bot.send_message(message.chat.id, msg)
+                    cursor.execute("UPDATE queue SET state = 1 WHERE state=0 and chat_id=? and task_id=?",
+                                   (message.chat.id, row[1],))
+
+            db.close()
+
+    bot.polling(none_stop=True, timeout=120)
 
 
 if __name__ == "__main__":
